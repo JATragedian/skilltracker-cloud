1b8a45f9de7b749da83c503228139cf82d3ac82e
JATragedian <JATragedian@yandex.ru>
2025-11-21 22:52:24 +0300

    Add Gateway Service

--- discovery-service/src/main/java/com/spb/skilltracker/cloud/discoveryservice/DiscoveryServiceApplication.java
+++ discovery-service/src/main/java/com/spb/skilltracker/cloud/discoveryservice/DiscoveryServiceApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 public class DiscoveryServiceApplication {
 
-	static void main(String[] args) {
+	public static void main(String[] args) {
 		SpringApplication.run(DiscoveryServiceApplication.class, args);
 	}
 
--- skill-service/src/main/java/com/spb/skilltracker/cloud/discoveryservice/SkillServiceApplication.java
+++ skill-service/src/main/java/com/spb/skilltracker/cloud/discoveryservice/SkillServiceApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 public class SkillServiceApplication {
 
-	static void main(String[] args) {
+	public static void main(String[] args) {
 		SpringApplication.run(SkillServiceApplication.class, args);
 	}
 
