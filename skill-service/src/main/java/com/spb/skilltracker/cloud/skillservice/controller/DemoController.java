98405c3472d7fff4e8c417de7526fc329170251c
JATragedian <JATragedian@yandex.ru>
2025-11-21 18:56:42 +0300

    Remove Local profile of config-service, add demo controller

---
+++ skill-service/src/main/java/com/spb/skilltracker/cloud/skillservice/controller/DemoController.java
@@ -0,0 +1,17 @@
+package com.spb.skilltracker.cloud.skillservice.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @Value("${example.message:NOT_LOADED}")
+    private String message;
+
+    @GetMapping("/config-test")
+    public String test() {
+        return message;
+    }
+}
